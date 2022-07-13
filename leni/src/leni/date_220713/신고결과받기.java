e3f9cb8d0e4a6c8186c811d48dcd271baba6dea3
leni-s <30317288+byLeni@users.noreply.github.com>
2022-07-14 00:32:21 +0900

    220713 - 신고하기... 업데이트 2ㅠ@ 으우에ㅔㄱ

--- leni/src/leni/date_220713/신고결과받기.java
+++ leni/src/leni/date_220713/신고결과받기.java
@@ -1,45 +1,10 @@
 package leni.date_220713;
 import leni.Solution;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java
  */
-
-class ReportInfo {
-
-    private final ArrayList<Integer> reportUserSeqList;
-    private int reportCount;
-
-    ReportInfo(){
-        this.reportUserSeqList = new ArrayList<>();
-        this.reportCount = 0;
-    }
-
-    public int getReportCount() {
-        return reportCount;
-    }
-
-    public void addReportUserSeqList(int userSeq) {
-        this.reportUserSeqList.add(userSeq);
-    }
-
-    public ArrayList<Integer> getReportUserSeqList() {
-        return reportUserSeqList;
-    }
-
-    public boolean isFirstReport(int userSeq) {
-        return !this.reportUserSeqList.contains(userSeq);
-    }
-
-    public void plusReportCount() {
-        this.reportCount++;
-    }
-}
+import java.util.*;
 
 public class 신고결과받기 implements Solution {
 
@@ -52,44 +17,38 @@ public class 신고결과받기 implements Solution {
         String[] id_list = {"muzi", "frodo", "apeach", "neo"}; // 유저 아이디 리스트
         String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}; // 유저 신고 히스토리 - format : 신고자 피신고자
         int k = 2; // 정지당하는 신고 횟수
-        System.out.print(Arrays.toString(this.solution(id_list, report, k)));
+        System.out.println(Arrays.toString(this.solution(id_list, report, k)));
 
          String[] id_list1 = {"con", "ryan"}; // 유저 아이디 리스트
         String[] report1 = {"ryan con", "ryan con", "ryan con", "ryan con"}; // 유저 신고 히스토리 - format : 신고자 피신고자
         int k1 = 3; // 정지당하는 신고 횟수
-        System.out.print(Arrays.toString(this.solution(id_list1, report1, k1)));
+        System.out.println(Arrays.toString(this.solution(id_list1, report1, k1)));
     }
 
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
 
-        Map<String, ReportInfo> reportHistory = new HashMap<String, ReportInfo>();
+        /* HashSet: 중복을 허용하지 않는 타입 implement set, null 허용 */
+        Map<String, HashSet<Integer>> reportHistory = new HashMap<>();
 
         for (String history: report ) {
-            String reportUser = history.split(" ")[0];
-            String blackUser = history.split(" ")[1];
+            /* stringTokenizer: split 을 대체하여 사용할 수 있다.
+            서로 약간의 차이점은 있으나 동작 상으로 봤을 때 stringTokenizer 가 좀 더 빠른 것 같음 */
+            StringTokenizer historyToken = new StringTokenizer(history);
+            String reportUser = historyToken.nextToken();
+            String blackUser = historyToken.nextToken();
 
-            ReportInfo reportInfo = reportHistory.get(blackUser);
-            if(reportInfo == null){
-                reportInfo = new ReportInfo();
-            }
+            /* getOrDefault: 있으면 첫번째 매개변수, 없으면 두번째 매개변수 값을 반환한다. */
+            HashSet<Integer> reportInfo = reportHistory.getOrDefault(blackUser, new HashSet<>());
 
             int userSeq = Arrays.asList(id_list).indexOf(reportUser);
-//            System.out.println(reportUser+" report "+blackUser+" : "+userSeq);
-            if(reportInfo.isFirstReport(userSeq)){
-                reportInfo.addReportUserSeqList(userSeq);
-                reportInfo.plusReportCount();
-            }
-
+            reportInfo.add(userSeq);
             reportHistory.put(blackUser, reportInfo);
         }
 
-        for(String blackUser: reportHistory.keySet()){
-            ReportInfo reportInfo = reportHistory.get(blackUser);
-            if(k <= reportInfo.getReportCount()){
-                for(int userSeq: reportInfo.getReportUserSeqList()){
-                    answer[userSeq]++;
-                }
+        for(HashSet<Integer> reportUserList: reportHistory.values()){
+            if(k <= reportUserList.size()){
+                reportUserList.forEach(idx -> answer[idx]++);
             }
         }
 
